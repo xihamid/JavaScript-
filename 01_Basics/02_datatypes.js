696cca9c0ca2a74781d69c8f387b7d22889e57f1
Hamid <68799266+xihamid@users.noreply.github.com>
2024-02-12 14:59:52 +0000

    DataTypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version (now all code will be as new version E6)
+
+// alert(3+3); // it will not work because we are working in node not in browser
+// JS ka sara engine browser may chupa howa hy.
+// code readabilty should be high
+
+let name="Hamid";
+let age=26;
+let isLoggedIN=false;
+
+// numbers => 2 to power 53   range
+//bignit    => mostly use is trading or stock exchange
+// bolloean    => just true or false
+// null   =>  standalone value (is is a special type) 
+// undefined  =>  (No value is assigned yet)    abi value assign nahi hoe hy
+// symbol   => symbol is mostly used for unique 
+//objects 
+
+console.log(typeof "Hamid");
+console.log(typeof age);
+console.log(typeof null); // type is object
+console.log(typeof undefined); // type is undefined because it is a type in JavaScript
