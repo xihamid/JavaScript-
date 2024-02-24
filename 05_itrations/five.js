55f7d26798e19b583cba53f980cab982c54a3915
Hamid <68799266+xihamid@users.noreply.github.com>
2024-02-24 08:20:16 +0000

    dom and itrations

---
+++ 05_itrations/five.js
@@ -0,0 +1,51 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+
+// coding.forEach((item)=>{
+// console.log(item);
+// })
+
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+
+
+
+
+
+// for each k pas item,index or arr mtl puri array b majood hoti
+
+
+// coding.forEach((item,index,arr)=>{
+// console.log(`index:${index}--item ${item}--------------arr:${arr}`);
+// })
+
+
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+
+
+
+
+myCoding.forEach((item,index)=>{
+
+    console.log(item.languageName);
+})
